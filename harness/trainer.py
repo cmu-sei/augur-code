cce947d6556bee764ccb85eb93ddb543870a2017
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-04-06 15:37:03 -0400

    Added mode to trainer to eval based on a given dataset

--- harness/trainer.py
+++ harness/trainer.py
@@ -1,3 +1,4 @@
+import sys
 
 import numpy as np
 import sklearn.model_selection as skm
@@ -27,7 +28,7 @@ def split_data(x_all, x_angle_all, y_all):
     return [x_train, x_angle_train, y_train, x_valid, x_angle_valid, y_valid]
 
 
-def train(model, x_train, x_angle_train, y_train, x_valid, x_angle_valid, y_valid):
+def fit(model, x_train, x_angle_train, y_train, x_valid, x_angle_valid, y_valid):
     file_path = ".model_weights.hdf5"
     callbacks = get_callbacks(filepath=file_path, patience=5)
 
@@ -39,14 +40,7 @@ def train(model, x_train, x_angle_train, y_train, x_valid, x_angle_valid, y_vali
     return history
 
 
-# Main code.
-def main():
-    np.random.seed(555)
-
-    # Load config.
-    config = Config()
-    config.load(CONFIG_FILENAME)
-
+def train(config):
     # Load and split data.
     dataset = augur_dataset.DataSet()
     dataset.load_data(config.get("dataset"))
@@ -59,11 +53,43 @@ def main():
     model.summary()
 
     # Train.
-    history = train(model, x_train, x_angle_train, y_train, x_valid, x_angle_valid, y_valid)
+    history = fit(model, x_train, x_angle_train, y_train, x_valid, x_angle_valid, y_valid)
     #plotter.show_results(history)
 
     # Save trained model.
-    augur_model.save_model_to_file(model, config.get("output"))
+    augur_model.save_model_to_file(model, config.get("model"))
+
+
+def evaluate(config):
+    # Load evaluation dataset.
+    dataset = augur_dataset.DataSet()
+    dataset.load_data(config.get("test"))
+
+    model = augur_model.load_model_from_file(config.get("model"))
+    print("Evaluate on test data", flush=True)
+    results = model.evaluate([dataset.x_combined_bands, dataset.x_angle], dataset.y_results, batch_size=128)
+    print("test loss, test acc:", results, flush=True)
+
+
+# Main code.
+def main():
+    np.random.seed(555)
+
+    # See if we'll use the default or a special config file.
+    config_file = CONFIG_FILENAME
+    if len(sys.argv) > 1:
+        config_file = str(sys.argv[1])
+        print('Config file top use: ', config_file)
+
+    # Load config.
+    config = Config()
+    config.load(config_file)
+
+    mode = config.get("mode")
+    if mode == "train":
+        train(config)
+    else:
+        evaluate(config)
 
 
 if __name__ == '__main__':
