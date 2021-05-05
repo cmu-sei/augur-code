1d1e7c28dfa9136c5914a997bb875e93e049086f
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-05-05 17:58:26 -0400

    Fixed interface name

--- harness/drifts/math_drift.py
+++ harness/drifts/math_drift.py
@@ -1,7 +1,7 @@
 import math
 
 
-def get_next_bin(index, curr_bin_idx, num_total_bins, params):
+def get_bin_index(index, curr_bin_idx, num_total_bins, params):
     """This only makes sense with num_total_bins is 2. Otherwise it will return non-alternating results."""
     if math.floor(math.sin(index)) == 0:
         return curr_bin_idx
