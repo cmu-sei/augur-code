b8691e8cc3e17ef765cb2432396044a7b289bfd4
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-05-05 18:00:28 -0400

    Comments

--- harness/drifts/math_drift.py
+++ harness/drifts/math_drift.py
@@ -2,7 +2,7 @@ import math
 
 
 def get_bin_index(index, curr_bin_idx, num_total_bins, params):
-    """This only makes sense with num_total_bins is 2. Otherwise it will return non-alternating results."""
+    """This only makes sense with num_total_bins is 2. Otherwise it will return alternating between 2 continguous indexes, increasing per timebox"""
     if math.floor(math.sin(index)) == 0:
         return curr_bin_idx
     else:
