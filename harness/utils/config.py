d6a29ea83e06087ded007d5732b71219ae22eaf0
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-07-13 13:42:33 -0400

    Improved configs obtained from command line to simplify experiment running

--- harness/utils/config.py
+++ harness/utils/config.py
@@ -26,34 +26,46 @@ class Config(object):
     def choose_from_folder(arguments, config_folder, default_config):
         """Processes command line arguments to allow for 1) choosing a config from a given folder, or
         2) to pass a config file name as a parameter."""
-        if len(arguments) > 1:
+        config_file = None
+        if len(arguments) < 2:
+            print("No command line arguments.")
+        else:
             command = str(arguments[1])
             if command == "--exp":
-                print("Please select one of the configuration files from the list:")
-                file_idx = 0
-                config_files = []
-                for root, dirs, files in os.walk(config_folder):
-                    for filename in files:
-                        file_idx = file_idx + 1
-                        config_files.append(filename)
-                        print(f"{file_idx}. {filename}")
-                try:
-                    option = int(input(f"Which file would you use? (Indicate a number between 1 and {file_idx}): "))
-                    if int(option) not in range(1, file_idx+1):
-                        print("No valid config file selected.")
-                    else:
-                        config_file = os.path.join(config_folder, config_files[option-1])
-                        print("Config file to use: ", config_file)
-                        return config_file
-                except ValueError as ex:
-                    # Assume we are getting a config file name.
-                    config_file = command
-                    print(f"Config file to use: {config_file}")
-                    return config_file
+                if len(arguments) >= 3:
+                    config_file = os.path.join(config_folder, str(arguments[2]))
+                    print(f"Using command line argument as config file name: {config_file}")
+                else:
+                    config_file = Config.get_file_option_from_user(config_folder)
             else:
                 print("No valid command line arguments found.")
-        else:
-            print("No command line arguments.")
 
-        print("Using default config: " + default_config)
-        return default_config
+        if config_file is None:
+            print("Using default config: " + default_config)
+            config_file = default_config
+
+        return config_file
+
+    @staticmethod
+    def get_file_option_from_user(config_folder):
+        print("Please select one of the configuration files from the list:")
+        file_idx = 0
+        config_files = []
+        for root, dirs, files in os.walk(config_folder):
+            for filename in files:
+                file_idx = file_idx + 1
+                config_files.append(filename)
+                print(f"{file_idx}. {filename}")
+        try:
+            option = int(input(f"Which file would you use? (Indicate a number between 1 and {file_idx}): "))
+        except ValueError as ex:
+            print("Invalid option selected.")
+            return None
+
+        if option not in range(1, file_idx+1):
+            print("No valid config file selected.")
+            return None
+        else:
+            config_file = os.path.join(config_folder, config_files[option-1])
+            print("Config file to use: ", config_file)
+            return config_file
