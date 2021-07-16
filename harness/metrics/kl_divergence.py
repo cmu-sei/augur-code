411ebb81c6d650ddea9671ccc147d755c645c054
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-07-16 15:47:38 -0400

    Updated pdf for kldiv to be calculated over possible values, and not actual data

--- harness/metrics/kl_divergence.py
+++ harness/metrics/kl_divergence.py
@@ -1,12 +1,18 @@
 import numpy as np
 from scipy.stats import norm
 
+RANGE_STEP = 0.001
+dist_range = None
+
 
 def metric_pdf(data, pdf_params):
     """Calculates the normal distribution on the dataset."""
+    global dist_range
+    if dist_range is None:
+        dist_range = np.arange(pdf_params.get("range_start"), pdf_params.get("range_end"), RANGE_STEP)
     mean = np.mean(data)
     std_dev = np.std(data)
-    return norm.pdf(data, mean, std_dev)
+    return norm.pdf(dist_range, mean, std_dev)
 
 
 def metric_reduction(probability_distribution):
