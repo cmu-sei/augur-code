c1358d05f033f8315d0edc78fb791ca0cb7abe36
jchrszcz <jchrabaszcz@gmail.com>
2021-08-20 11:47:04 -0400

    removed range calculation as unnecessary

--- harness/metrics/hellinger.py
+++ harness/metrics/hellinger.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 
-RANGE_STEP = 0.001
-dist_range = None
-
 def metric_distance(p, q):
     """Calculates Hellinger distance."""
     hellinger_dist = np.sqrt(np.where(p != 0, np.sum((np.sqrt(p) - np.sqrt(q)) ** 2), 0)) / np.sqrt(2)
