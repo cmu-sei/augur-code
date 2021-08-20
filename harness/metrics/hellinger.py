7773c2ea253f77a506f4a6642f8a52ef9a5d6801
jchrszcz <jchrabaszcz@gmail.com>
2021-08-20 12:15:16 -0400

    fixed transposition error, added test for return dimension

--- harness/metrics/hellinger.py
+++ harness/metrics/hellinger.py
@@ -3,6 +3,13 @@ import numpy as np
 
 def metric_distance(p, q):
     """Calculates Hellinger distance."""
-    hellinger_dist = np.sqrt(np.where(p != 0, np.sum((np.sqrt(p) - np.sqrt(q)) ** 2), 0)) / np.sqrt(2)
+    hellinger_dist = np.sqrt(np.sum(np.where(p != 0, (np.sqrt(p) - np.sqrt(q)) ** 2, 0))) / np.sqrt(2)
     print(f"Hellinger Distance: {hellinger_dist}")
     return hellinger_dist
+
+if __name__ == "__main__":
+    n = 100
+    p = np.random.random(n)
+    q = np.random.random(n)
+    hellinger_dist = metric_distance(p,q)
+    print(f"Length of return is {hellinger_dist.size}")
