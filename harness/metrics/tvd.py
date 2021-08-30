98a02ca00c9ea4ca15b2437c98d2b3c58774817f
jchrszcz <jchrabaszcz@gmail.com>
2021-08-30 13:04:50 -0400

    added wasserstein and added test for total variation

--- harness/metrics/tvd.py
+++ harness/metrics/tvd.py
@@ -1,16 +1,20 @@
 import numpy as np
+from scipy.stats import wasserstein_distance
 
 
 def metric_distance(p, q):
     """Calculates total variation distance."""
-    tvd = np.sum(np.abs(p - q)) / 2
+    tvd = np.mean(np.abs(p - q)) / 2
     print(f"Total variation distance: {tvd}")
     return tvd
 
 if __name__ == "__main__":
-    n = 100
+    n = 1000
     p = np.random.random(n)
     q = np.random.random(n)
+    p = p / np.sum(p)
+    p = q / np.sum(q)
     tvd = metric_distance(p,q)
+    print(f"Half the Wasserstein Distance: {wasserstein_distance(p,q) / 2}")
     print(f"Length of return is {tvd.size}")
     
