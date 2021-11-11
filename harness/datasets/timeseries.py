Adding density calculation for new time-series distance metrics, and adding pdf_params result for a time_series. Also fixing base structure for error metrics

harness/datasets/timeseries.py
@@ -42,18 +42,35 @@ class TimeSeries:
     time_intervals = np.empty(0, dtype=int)
     aggregated = np.empty(0)
     pdf = np.empty(0)
+    pdf_params = np.empty(0)
 
     def get_time_intervals(self):
         return self.time_intervals
 
-    def get_aggregated(self):
-        return self.aggregated
+    def get_aggregated(self, time_interval_id=None):
+        """Returns either the full array of aggregated values, or a specific one by the time interval index."""
+        if time_interval_id is None:
+            return self.aggregated
+        else:
+            if time_interval_id > len(self.aggregated):
+                return self.aggregated[time_interval_id]
+            else:
+                raise Exception(f"Invalid time interval id passed: {time_interval_id}, length is {len(self.aggregated)}")
 
-    def get_pdf(self):
-        return self.pdf
+    def get_pdf(self, time_interval_id):
+        if time_interval_id > len(self.pdf):
+            return self.pdf[time_interval_id]
+        else:
+            raise Exception(f"Invalid time interval id passed: {time_interval_id}, length is {len(self.pdf)}")
 
     def set_pdf(self, pdf):
         self.pdf = pdf
+
+    def get_pdf_params(self, time_interval_id):
+        if time_interval_id > len(self.pdf_params):
+            return self.pdf_params[time_interval_id]
+        else:
+            raise Exception(f"Invalid time interval id passed: {time_interval_id}, length is {len(self.pdf_params)}")
 
     def get_model_input(self):
         """Returns the time intervals and aggregated values as a model input."""
@@ -64,13 +81,15 @@ class TimeSeries:
         self.time_intervals = np.arange(start_time_interval, num_intervals)
         self.aggregated = np.zeros(self.time_intervals.size)
         self.pdf = np.zeros(self.time_intervals.size)
+        self.pdf_params = np.zeros(self.time_intervals.size)    #TODO: check this
 
-    def add_data(self, time, aggregated_value, pdf=None):
+    def add_data(self, time, aggregated_value, pdf=None, pdf_params=None):
         """Adds aggregated data to the given time position."""
         if time in self.time_intervals:
             idx = np.where(self.time_intervals == time)
             self.aggregated[idx] = aggregated_value
             self.pdf[idx] = pdf
+            self.pdf_params[idx] = pdf_params
         else:
             raise Exception(f"Invalid time index, not found in times array: {time}")
 

harness/metrics/base_metric.py
@@ -1,5 +1,10 @@
 import importlib
 
+import numpy as np
+from scipy.stats import norm
+
+from utils.logging import print_and_log
+
 
 def load_metric_module(module_name):
     """Loads a metric module given the name"""
@@ -29,14 +34,14 @@ def check_module_loaded(metric_module):
 class Metric:
     """Generic Metric class, will load specific metric module as required."""
     metric_module = None
-    metric_params = None
+    config_params = None
     time_series = None
     ts_predictions = None
 
     def load_metric_functions(self, config):
         """Loads a metric module from config."""
         self.metric_module = load_metric_module(config.get("module"))
-        self.metric_params = config.get("params")
+        self.config_params = config.get("params")
 
     def initial_setup(self, time_series, ts_predictions):
         """Method to be called once before starting to work with this metric."""
@@ -66,17 +71,75 @@ class ErrorMetric(Metric):
             return self.metric_module.metric_error(self.time_interval_id, self.time_series, self.ts_predictions)
 
 
+class DensityEstimator:
+    """Implements most common density functions."""
+
+    # Helper array with range of potential valid values used to calculate distributions.
+    DIST_RANGE_STEP = 0.001
+    dist_range = None
+    config_params = None
+
+    def __init__(self, config_params, metric_module):
+        """Gets the config params and sets up the dist range."""
+        self.config_params = config_params
+        self.metric_module = metric_module
+        self.setup_valid_range()
+
+    def setup_valid_range(self):
+        """ Compute the dist range based the configuration."""
+        if self.dist_range is None:
+            range_start = self.config_params.get("range_start")
+            range_end = self.config_params.get("range_end")
+            print_and_log(f"Range: {range_start} to {range_end}")
+            self.dist_range = np.arange(range_start, range_end, self.DIST_RANGE_STEP)
+
+    def calculate_probability_distribution(self, data, metric_params):
+        """Calculates and returns the probability distribution for the given data."""
+        distribution = self.config_params.get("distribution")
+        print_and_log(f"Using distribution: {distribution}")
+        if distribution == "custom":
+            if check_module_loaded(self.metric_module):
+                return self._calculate_custom_dist(data, metric_params, self.metric_module)
+        elif distribution == "normal":
+            return self._calculate_normal_dist(data, metric_params)
+        else:
+            raise Exception(f"Unsupported distribution type: {distribution}")
+
+    def _calculate_custom_dist(self, data, metric_params):
+        """Calls custom density function implemented in defined module."""
+        try:
+            print_and_log("Using custom density function.")
+            return self.metric_module.metric_density(data, self.dist_range, metric_params, self.config_params)
+        except AttributeError:
+            error_msg = "Custom distribution density function not implemented, aborting."
+            print_and_log(error_msg)
+            raise Exception(error_msg)
+
+    def _calculate_normal_dist(self, data, params):
+        """Normal dist calculation."""
+        mean = np.mean(data)
+        std_dev = params.get("std_dev")
+        print_and_log(f"Mean: {mean}, Std Dev: {std_dev}")
+        return norm.pdf(self.dist_range, mean, std_dev)
+
+
 class DistanceMetric(Metric):
     """Implements a distance-based metric that can load metric-specific functions from a config."""
     prev_probability_distribution = []  # P
     curr_probability_distribution = []  # Q
+    density_estimator = None
+
+    def initial_setup(self, time_series, ts_predictions):
+        """Overriden."""
+        super().initial_setup(time_series, ts_predictions)
+        self.density_estimator = DensityEstimator(self.config_params, self.metric_module)
 
     def step_setup(self, time_interval_id):
         """Overriden."""
         # Calculate the probability distribution for the time interval.
-        # TODO: How to calculate P.
-        self.prev_probability_distribution = TODO
-        self.curr_probability_distribution = self.ts_predictions.get_pdf()[time_interval_id]
+        self.prev_probability_distribution = self.density_estimator.calculate_probability_distribution(self.time_series.get_aggregated(),
+                                                                                                       self.time_series.get_pdf_params(time_interval_id))
+        self.curr_probability_distribution = self.ts_predictions.get_pdf(time_interval_id)
 
     def calculate_metric(self):
         """Calculates the distance defined for the current prob dist and the reference one."""

harness/metrics/kolmogorov_smirnov.py
@@ -2,9 +2,11 @@ import numpy as np
 from scipy.stats import ks_2samp
 
 
-def metric_error(sample_group, dataset=None, predictions=None):
+def metric_error(time_interval_id, time_series, ts_predictions):
     """Calculates the Kolmogorov-Smirnov error."""
-    ks_stat = ks_2samp(sample_group.get_expected_results(), sample_group.get_predictions()).statistic
+    # TODO: Fix this error metric.
+    #ks_stat = ks_2samp(sample_group.get_expected_results(), sample_group.get_predictions()).statistic
+    ks_stat = 0
     print(f"KS Test: {ks_stat}")
     return ks_stat
 

harness/metrics/stepd.py
@@ -1,13 +1,9 @@
-import numpy as np
 
 
-def metric_error(sample_group, dataset, predictions):
+def metric_error(time_interval, time_series, ts_predictions):
     """Calculates the STEPD error."""
-    r0 = np.count_nonzero(np.array_equal(dataset.get_output(), predictions))
-    n0 = dataset.get_number_of_samples()
-    rr = np.count_nonzero(sample_group.get_correctness())
-    nr = sample_group.get_number_of_samples()
-
-    p_hat = (r0 + rr) / (n0 + nr)
-    t = abs((r0 / rr) - (n0 / nr)) - 0.5 * (1 / n0 + 1 / nr) / np.sqrt(p_hat * (1 - p_hat) * (1 / n0 + 1 / nr))
+    x1 = time_series.get_aggregated(time_interval)
+    x2 = ts_predictions.get_pdf_params(time_interval).get("mean")
+    sigma = ts_predictions.get_pdf_params(time_interval).get("std_dev")
+    t = (x1 - x2) / sigma
     return t

harness/metrics/z_test.py
@@ -2,9 +2,11 @@ import numpy as np
 from scipy.stats import ttest_ind
 
 
-def metric_error(sample_group, dataset=None, predictions=None):
+def metric_error(time_interval_id, time_series, ts_predictions):
     """Calculates the z test statistic."""
-    z_stat = ttest_ind(sample_group.get_expected_results(), sample_group.get_predictions()).statistic
+    # TODO: Fix this error metric.
+    #z_stat = ttest_ind(sample_group.get_expected_results(), sample_group.get_predictions()).statistic
+    z_stat = 0
     print(f"Z Test: {z_stat}")
     return z_stat
 