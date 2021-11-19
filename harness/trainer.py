3ad5288e75b8fcee358ad61adfd1b20089548e66
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-11-19 14:18:25 -0500

    Clarity

--- harness/trainer.py
+++ harness/trainer.py
@@ -51,24 +51,25 @@ def main():
         default_evaluation_output = dataset_instance.get_output()
         main_trainer.evaluate(trained_model, default_evaluation_input, default_evaluation_output)
     if CONFIG.get("time_series_training") == "on":
+        time_interval_params = CONFIG.get("time_interval")
         time_series = TimeSeries()
         if dataset_instance.has_timestamps():
             # If the dataset comes with timestamps, aggregate using them.
-            time_series.aggregate_by_timestamp(CONFIG.get("time_interval").get("starting_interval"),
-                                               CONFIG.get("time_interval").get("interval_unit"),
+            time_series.aggregate_by_timestamp(time_interval_params.get("starting_interval"),
+                                               time_interval_params.get("interval_unit"),
                                                dataset_instance,
                                                dataset_instance.get_output())
         else:
             # If the dataset doesn't have timestamps, use the samples_per_interval config to aggregate.
             samples_per_interval = int(CONFIG.get("time_interval").get("samples_per_time_interval"))
-            time_series.aggregate_by_number_of_samples(CONFIG.get("time_interval").get("starting_interval"),
-                                                       CONFIG.get("time_interval").get("interval_unit"),
+            time_series.aggregate_by_number_of_samples(time_interval_params.get("starting_interval"),
+                                                       time_interval_params.get("interval_unit"),
                                                        dataset_instance,
                                                        dataset_instance.get_output(),
                                                        samples_per_interval)
         trained_model = timeseries_model.create_fit_model(time_series.get_time_intervals(),
                                                           time_series.get_aggregated(),
-                                                          CONFIG.get("time_interval").get("interval_unit"),
+                                                          time_interval_params.get("interval_unit"),
                                                           CONFIG.get("ts_hyper_parameters"))
         timeseries_model.save(trained_model, CONFIG.get("ts_model"))
 
