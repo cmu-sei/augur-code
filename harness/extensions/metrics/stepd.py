a0a73bc0e3b862e3d12c95b79abc2ce77fb709c5
jchrszcz <jchrabaszcz@gmail.com>
2021-11-30 15:05:24 -0500

    fixed error metrics for one-sample, removed stepd (no longer applies)

--- harness/extensions/metrics/stepd.py
+++
@@ -1,9 +0,0 @@
-
-
-def metric_error(time_interval, time_series, ts_predictions):
-    """Calculates the STEPD error."""
-    x1 = time_series.get_aggregated(time_interval)
-    x2 = ts_predictions.get_pdf_params(time_interval).get("mean")
-    sigma = ts_predictions.get_pdf_params(time_interval).get("std_dev")
-    t = (x1 - x2) / sigma
-    return t
--- harness/extensions/metrics/z_test.py
+++ harness/extensions/metrics/z_test.py
@@ -1,30 +1,8 @@
-import numpy as np
-from scipy.stats import ttest_ind
 
-
-def metric_error(time_interval_id, time_series, ts_predictions):
-    """Calculates the z test statistic."""
-    # TODO: Fix this error metric.
-    #z_stat = ttest_ind(sample_group.get_expected_results(), sample_group.get_predictions()).statistic
-    z_stat = 0
-    print(f"Z Test: {z_stat}")
-    return z_stat
-
-
-if __name__ == "__main__":
-    class TestSampleGroup:
-
-        def __init__(self, samples):
-            self.samples = samples
-
-        def get_expected_results(self):
-            return np.random.random(self.samples)
-
-        def get_predictions(self):
-            return np.random.random(self.samples)
-
-    n = 100
-    sample_group = TestSampleGroup(n)
-    z_stat = metric_error(sample_group)
-    print(f"Length of return is {np.array(z_stat).size}")
-    
+def metric_error(time_interval, time_series, ts_predictions):
+    """Calculates the STEPD error."""
+    x1 = time_series.get_aggregated(time_interval)
+    x2 = ts_predictions.get_pdf_params(time_interval).get("mean")
+    sigma = ts_predictions.get_pdf_params(time_interval).get("std_dev")
+    t = (x1 - x2) / sigma
+    return t
