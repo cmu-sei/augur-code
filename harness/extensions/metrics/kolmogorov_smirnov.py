bee2b4097bc75ea12f7195bafa94e90b24a5d41e
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-11-30 16:17:10 -0500

    Fixed variable name bug and also made them consistent

--- harness/extensions/metrics/kolmogorov_smirnov.py
+++ harness/extensions/metrics/kolmogorov_smirnov.py
@@ -1,11 +1,12 @@
 from scipy import stats
 
+
 def metric_error(time_interval_id, time_series, ts_predictions):
     """Calculates the Kolmogorov-Smirnov error."""
 
-    x = time_series.get_aggregated(time_interval)
-    loc = ts_predictions.get_pdf_params(time_interval).get("mean")
-    scale = ts_predictions.get_pdf_params(time_interval).get("std_dev")
+    x = time_series.get_aggregated(time_interval_id)
+    loc = ts_predictions.get_pdf_params(time_interval_id).get("mean")
+    scale = ts_predictions.get_pdf_params(time_interval_id).get("std_dev")
     
     ks_stat = stats.kstest([x], "norm", args=(loc, scale)).statistic
     return ks_stat
--- harness/extensions/metrics/z_test.py
+++ harness/extensions/metrics/z_test.py
@@ -1,8 +1,8 @@
 
-def metric_error(time_interval, time_series, ts_predictions):
+def metric_error(time_interval_id, time_series, ts_predictions):
     """Calculates the STEPD error."""
-    x1 = time_series.get_aggregated(time_interval)
-    x2 = ts_predictions.get_pdf_params(time_interval).get("mean")
-    sigma = ts_predictions.get_pdf_params(time_interval).get("std_dev")
+    x1 = time_series.get_aggregated(time_interval_id)
+    x2 = ts_predictions.get_pdf_params(time_interval_id).get("mean")
+    sigma = ts_predictions.get_pdf_params(time_interval_id).get("std_dev")
     t = (x1 - x2) / sigma
     return t
